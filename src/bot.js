aab1b35e4cc8e4cd1a652e2bf431c341b3b08bfb
MPH <misha.medvedev2001@gmail.com>
2020-07-25 19:33:24 +0300

    Now bot can send multiple chunks

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,7 @@ const TelegramBot = require('node-telegram-bot-api')
 const validator = require('validator')
 const ytdl = require('ytdl-core')
 const fs = require('fs')
+const splitFile = require('split-file')
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true })
 bot.onText(/\/start/, async (msg, match)=> {
     await bot.sendMessage(msg.chat.id, "Welcome!")
@@ -10,5 +11,10 @@ bot.on('message', async msg => {
     const text = msg.text.toString()
     if(!validator.isURL(text))
         return await bot.sendMessage('Not URL, please try another')
-    ytdl(text).pipe(fs.createWriteStream('someVideo.mp4')).on("close", async () => bot.sendVideo(msg.chat.id, './someVideo.mp4').then(() => fs.unlinkSync('someVideo.mp4')))
+    ytdl(text).pipe(fs.createWriteStream('someVideo.mp4')).on("close", async () => {
+        splitFile.splitFileBySize('someVideo.mp4', 40000000).then((names) => {
+            Promise.all(names.map(name => bot.sendDocument(msg.chat.id, name).then(() => fs.unlinkSync(name))))
+                .then(() => fs.unlinkSync('someVideo.mp4'))
+        })
+    })
 })
